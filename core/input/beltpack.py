b574654ef4f8c6d6fe5828df8faac8bd7d311941
guardmansduck <100035465@mvla.net>
2025-10-24 13:56:45 -0700

    Add Beltpack class for button press simulation
    Implements a Beltpack class to simulate button presses and register callbacks.

---
+++ core/input/beltpack.py
@@ -0,0 +1,28 @@
+from typing import Callable
+
+class Beltpack:
+    """
+    Simulates a beltpack input device.
+    Can trigger callbacks when buttons are "pressed".
+    """
+
+    def __init__(self):
+        # Dictionary of button_name -> callback
+        self._callbacks = {}
+
+    def register_button(self, button_name: str, callback: Callable):
+        """
+        Register a callback for a button press.
+        :param button_name: e.g., 'channel_up', 'channel_down', 'mute'
+        :param callback: function to call when button is pressed
+        """
+        self._callbacks[button_name] = callback
+
+    def press_button(self, button_name: str):
+        """
+        Simulate a beltpack button press.
+        """
+        if button_name in self._callbacks:
+            self._callbacks[button_name]()
+        else:
+            print(f"[Warning] No callback registered for button: {button_name}")
